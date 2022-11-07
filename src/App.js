9d3549f410d477194e33889d6722d6eede20ce44
Rod Bennett <rodbennett@Rods-Air-2.lan>
2022-11-06 19:27:37 -0700

    add new task method running

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
+import AddTask from './components/AddTask'
 
 
 const App = () => {
@@ -26,24 +27,39 @@ const App = () => {
   },
   ])
 
-  //delete task
+  // add task function
+  const addTask = (task) => {
+    // create random number for new task id
+    const id = Math.floor(Math.random() * 
+    10000) + 1
+    
+    // create variable for newTask, which takes in random id and the spread operator for task (...task) to includes all proerties of task 
+    const newTask = { id, ...task }
+    setTasks([ ...tasks, newTask ])
+    console.log(newTask)
+  }
+  //delete task function
   const deleteTask = (id) => {
     console.log("delete", id)
-    // this line says show all the tasks that are NOT equal to the id we've selected
+    // this line says show all the tasks that are NOT equal to the id we've selected. Use Set Tasks for any changing states.
    setTasks(tasks.filter((task) => task.id !==id))
   }
 
-  // Toggle reminder
+  // Toggle reminder function
   const toggleReminder = (id) => {
-    setTasks(tasks.map((task) => task.id === id ? {...task, reminder: !task.reminder} : task))
+
+    // maps tasks
+    setTasks(tasks.map((task) => 
+    // ternary conditional
+    task.id === id ? {...task, reminder: !task.reminder} : task))
   }
 
   return (
     <div className="container">
       <Header title="Task Tracker"/>
+      <AddTask onAdd={addTask} />
       {/* <Tasks tasks={tasks} onDelete={deleteTask}/> */}
       {/* if there are no tasks, create a ternary method to say "no tasks": */}
-
       {tasks.length > 0 ? (
         <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/> 
         ) : ('No tasks to show' 
--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,18 +1,61 @@
-const AddTask = () => {
+import { useState } from 'react'
+
+// onAdd desctructured parameter from 
+const AddTask = ({ onAdd }) => {
+    //sets the proerties of tasks ans well as set variables for state changes
+    const [text, setText] = useState('')
+    const [day, setDay] = useState('')
+    const [reminder, setReminder] = useState(false)
+
+    const onSubmit = (e) => {
+        e.preventDefault()
+
+        if (!text) {
+            alert("Please add a task")
+            return
+        }
+        // sets form back to empty after submit
+    onAdd({ text, day, reminder })
+    setText('')
+    setDay('')
+    setReminder(false)
+    }
+
   return (
-    <form className="add-form">
+    <form className="add-form" onSubmit={onSubmit}>
         <div className="form-control">
             <label>Task</label>
-            <input type='text' placeholder='Add task' />
+            <input type='text' placeholder='Add task' 
+
+            // user value set to {text} as in our model array
+            value={text} onChange={(e) => 
+
+            // setText used for changing state. New {text} value of user passed as event into onChange(e)
+            setText(e.target.value)}/>
         </div>
         <div className="form-control">
             <label>Day & Time</label>
-            <input type='text' placeholder='Add task' />
+            <input type='text' placeholder='Add day and time' 
+            // user value set to {day} as in our model array
+            value={day} onChange={(e) => 
+
+            // setDay used for changing state. New {day} value of user passed as event into onChange(e)
+            setDay(e.target.value)}/>
         </div>
-        <div className="form-control">
+        <div className="form-control form-control-check">
             <label> Set Reminder</label>
-            <input type='text' placeholder='Add task' />
+            <input type='checkbox' 
+
+            // if reminder checkbox is checked, set to true
+            checked={reminder}
+
+            // new {reminder} value from user sets to ture if box is checked
+            value={reminder} onChange={(e) => 
+
+            // setreminder used for changing state. New {reminder} value of user passed as event into onChange(e) if box is checked
+            setReminder(e.currentTarget.checked)}/>
         </div>
+        <input type='submit' value='Save Task' className="btn btn-block" />
     </form>
   )
 }
